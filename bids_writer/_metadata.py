5510f90565809471e545584419b22980b63a1864
Nate Vack <njvack@wisc.edu>
2015-11-20 14:16:20 -0600

    Add metadata

---
+++ bids_writer/_metadata.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+version = "0.1.0"
+author = "Nathan Vack"
+author_email = "njvack@wisc.edu"
+license = "MIT"
+copyright = "Copyright 2015 Boards of Regent of the University of Wisconsin System"
+url = "https://github.com/njvack/bids-json-writer"
