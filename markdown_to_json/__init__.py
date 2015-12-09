dfb3dacccbc733f81203324f6c5cc348d09152c5
Nate Vack <njvack@wisc.edu>
2015-12-09 12:27:29 -0600

    Fix absolute imports

--- markdown_to_json/__init__.py
+++ markdown_to_json/__init__.py
@@ -1,2 +1,3 @@
-# -*- coding: utf-8 -*-
-from _metadata import version as __version__, author as __author__
+from __future__ import absolute_import
+
+from ._metadata import version as __version__, author as __author__
