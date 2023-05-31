a0db8ee839810cafa3c818e6fb88369b478986ee
Matthew Martin <matthewdeanmartin@gmail.com>
2023-05-31 18:15:54 -0400

    issue 4-- result not sensible

---
+++ examples/mixed/issue_4.md
@@ -0,0 +1,27 @@
+## GUID
+
+2db62bb2-8ac0-4137-b26f-78a12bff449d
+
+## Title
+
+Some title
+
+## Summary
+
+A Summary
+## Priority
+
+- Must
+
+## Detailed Description
+
+- Lorem Ipsum Blabla
+- Lorem Ipsum Blabla 
+
+## Reference Requirements
+
+> na
+
+## Categories
+
+- Cat6
--- tests/test_mixed_content.py
+++ tests/test_mixed_content.py
@@ -18,3 +18,40 @@ def test_mixed():
             dictionary = CMarkASTNester().nest(ast)
             stringified = Renderer().stringify_dict(dictionary)
             assert stringified
+
+
+def test_issue_4():
+    value = """## GUID
+
+2db62bb2-8ac0-4137-b26f-78a12bff449d
+
+## Title
+
+Some title
+
+## Summary
+
+A Summary
+
+## Priority
+
+- Must
+
+## Detailed Description
+
+- Lorem Ipsum Blabla
+- Lorem Ipsum Blabla 
+
+## Reference Requirements
+
+> na
+
+## Categories
+
+- Cat6
+"""
+    ast = CommonMark.DocParser().parse(value)
+    dictionary = CMarkASTNester().nest(ast)
+    stringified = Renderer().stringify_dict(dictionary)
+    # Result isn't sensible though...
+    assert stringified
